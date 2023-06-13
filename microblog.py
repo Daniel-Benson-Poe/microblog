8122ef1a5345430c0cbea55703e63a546a6283f2
Poenomaly <djbenson741@gmail.com>
2023-06-13 15:18:42 -0600

    Add shell context for db import in flask shell command

--- microblog.py
+++ microblog.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+# Running shell command allows you to import processes without having to manually import them every time
+@app.shell_context_processor # registers function as a shell context function; invoked when flask shell command runs
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post} # dictionary returned because for each item you must also provide a name under which it will be refrenced in the shell, here given by the dictionary keys
